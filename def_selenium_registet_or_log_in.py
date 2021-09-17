b6ca5b84ba4b57d0e539162fb77c5b1e029f04ca
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-17 19:01:12 +0300

    .

---
+++ def_selenium_registet_or_log_in.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+# text_email_error = 'This email address is already being used by another account. Use a different email.'
+
+
+driver = webdriver.Chrome(executable_path='/home/vladyslav/IT_Step/lesson_25-26/chromedriver')
+
+def func_register():
+     try:
+
+          driver.get('https://pypi.org/account/register/')
+
+          input_full_name = driver.find_element_by_id('full_name')
+          input_full_name.clear()
+          input_full_name.send_keys('Tester')
+
+          input_email = driver.find_element_by_id('email')
+          input_email.clear()
+          input_email.send_keys('te433450@gmail.com')
+
+          input_username = driver.find_element_by_id('username')
+          input_username.clear()
+          input_username.send_keys('dolvar')
+
+          input_password = driver.find_element_by_id('new_password')
+          input_password.clear()
+          input_password.send_keys('1995V|_ad@2021')
+
+          input_password_confirm = driver.find_element_by_id('password_confirm')
+          input_password_confirm.send_keys('1995V|_ad@2021')
+          input_password_confirm.send_keys(Keys.ENTER)
+
+          check_errors = driver.find_element_by_id('email-errors')
+          # print(check_errors.text)
+          # if text_email_error in check_errors:
+          #      print('buy')
+
+     except Exception as ex:
+          print(ex)
+
+     finally:
+          time.sleep(5)
+          driver.close()
+
+
+# def func_log_in():
+
+#      try:
+
+#           driver.get('https://pypi.org/account/login/')
+
+#           input_username = driver.find_element_by_id('username')
+#           input_username.clear()
+#           input_username.send_keys('dolvar')
+
+#           input_password = driver.find_element_by_id('password')
+#           input_password.clear()
+#           input_password.send_keys('1995V|_ad@2021')
+          
+#           input_password.send_keys(Keys.ENTER)
+
+#      except Exception as ex:
+#           print(ex)
+
+#      # finally:
+#      #      time.sleep(5)
+#      #      driver.close()
+
+
+# func_log_in()
+func_register()
