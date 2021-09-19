dca3957970ef59808dc47845fc9fdf5784220416
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-09-19 18:06:36 +0300

    .

--- selenium_prom.py
+++ selenium_prom.py
@@ -16,7 +16,7 @@ def main_page():
           time.sleep(3)
 
           number_input_field = driver.find_element_by_id('phone_field')
-          number_input_field.send_keys('Input your phone number:')
+          number_input_field.send_keys(input('Input your phone number (+380):'))
 
           button_lod_in = driver.find_element_by_id('phoneEmailConfirmButton')
           button_lod_in.send_keys(Keys.RETURN)
@@ -25,8 +25,8 @@ def main_page():
           input_number_ = driver.find_elements_by_class_name('_1KcTA.O43e6._3iNcs._1zv6i._2qzjN._1aVgW')
           time.sleep(2)
 
-          for n in input_number_:
-               n.send_keys(input('Input code from your sms:'))
+          for items in input_number_:
+               items.send_keys(input('Input code from your sms:'))
 
           time.sleep(3)
 
@@ -69,18 +69,17 @@ def writing_comment():
 
           button_create_company_opinion = driver.find_element_by_class_name('VspXp.aEPJa._3dQ3K.Keuxg')
           button_create_company_opinion.send_keys(Keys.ENTER)
-
           time.sleep(3)
 
-#Здесь я думаю, как поставить 5 звезд
+          button_rating_label = driver.find_elements_by_css_selector('div._1KcTA.ggTpW._3TpPX._2wbE4.nN6NX._1v-n_')[4]
+          button_rating_label.click()
+          time.sleep(3)
 
-          # button_rating_label = driver.find_elements_by_class_name('_1KcTA.ggTpW._3TpPX._2wbE4.nN6NX._1v-n_')[4]
-          # print(button_rating_label)
-          # time.sleep(3)
-          # button_rating_label.send_keys(Keys.ENTER)
+          button_checkbox = driver.find_elements_by_class_name('_3NYl6')[0]
+          button_checkbox.click()
 
           input_opinion_text = driver.find_element_by_class_name('_3Gj6W._1mkhj')
-          input_opinion_text.send_keys('Все очень хорошо')
+          input_opinion_text.send_keys(input('Input your opinion_text:'))
 
      except Exception as ex:
           print(ex)
