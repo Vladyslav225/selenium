0818c7a6b6dfc6816de4102acc7d3c441c8dbb89
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-10-13 15:57:44 +0300

    .

--- selenium_prom.py
+++ selenium_prom.py
@@ -16,7 +16,9 @@ def main_page_search():
           search_term.send_keys(Keys.RETURN)
           time.sleep(3)
 
-          products = driver.find_elements_by_class_name('_2KaCs.xuuH_')[2]
+          products = driver.find_elements_by_class_name('_2KaCs.xuuH_._3y8C0')[1]
+          # print(products.text)
+
           products.send_keys(Keys.ENTER)
           time.sleep(3)
 
@@ -28,35 +30,35 @@ def writing_comment():
 
      try:
      
-          comany_name = driver.find_element_by_class_name('_2KaCs.fxzyv._3y8C0.N4J0p')
+          comany_name = driver.find_elements_by_class_name('_2KaCs._2RuOC')
           comany_name.send_keys(Keys.ENTER)
-          time.sleep(3)
+          time.sleep(5)
 
-          short_company_rating = driver.find_elements_by_class_name('_2KaCs._2RuOC')[1]
-          short_company_rating.send_keys(Keys.ENTER)
-          time.sleep(3)
+          # short_company_rating = driver.find_elements_by_class_name('_2KaCs._2RuOC')[1]
+          # short_company_rating.send_keys(Keys.ENTER)
+          # time.sleep(3)
 
-          button_create_company_opinion = driver.find_element_by_class_name('VspXp.aEPJa._3dQ3K.Keuxg')
-          button_create_company_opinion.send_keys(Keys.ENTER)
-          time.sleep(3)
+          # button_create_company_opinion = driver.find_element_by_class_name('VspXp.aEPJa._3dQ3K.Keuxg')
+          # button_create_company_opinion.send_keys(Keys.ENTER)
+          # time.sleep(3)
 
-          number_input_field = driver.find_element_by_id('phone_field')
-          number_input_field.send_keys(input('Input your phone number (+380):'))
+          # number_input_field = driver.find_element_by_id('phone_field')
+          # number_input_field.send_keys(input('Input your phone number (+380):'))
 
-          button_lod_in = driver.find_element_by_id('phoneConfirmButton')
-          button_lod_in.send_keys(Keys.RETURN)
-          time.sleep(3)
+          # button_lod_in = driver.find_element_by_id('phoneConfirmButton')
+          # button_lod_in.send_keys(Keys.RETURN)
+          # time.sleep(3)
 
-          input_number_ = driver.find_elements_by_class_name('_1KcTA.O43e6._3iNcs._1zv6i._2qzjN._1aVgW')
-          time.sleep(2)
+          # input_number_ = driver.find_elements_by_class_name('_1KcTA.O43e6._3iNcs._1zv6i._2qzjN._1aVgW')
+          # time.sleep(2)
 
-          for items in input_number_:
-               items.send_keys(input('Input code from your sms:'))
+          # for items in input_number_:
+          #      items.send_keys(input('Input code from your sms:'))
 
           # time.sleep(3)
 
-#           button_confirmation = driver.find_element_by_id('verifyTempPasswordConfirmButton')
-#           button_confirmation.send_keys(Keys.RETURN)
+          # button_confirmation = driver.find_element_by_id('verifyTempPasswordConfirmButton')
+          # button_confirmation.send_keys(Keys.RETURN)
 
 # #           button_rating_label = driver.find_elements_by_css_selector('div._1KcTA.ggTpW._3TpPX._2wbE4.nN6NX._1v-n_')[4]
 # #           button_rating_label.click()
@@ -71,8 +73,8 @@ def writing_comment():
      except Exception as ex:
           print(ex)
 
-     finally:
-          time.sleep(10)
+     # finally:
+     #      time.sleep(10)
           # driver.close()
 
 
