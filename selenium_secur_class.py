07606ecd31725c322aea644f7d2b31f6dab0dc96
Yarynovskiy Vladyslav <yvv.work@gmail.com>
2021-10-18 19:31:52 +0300

    .

--- selenium_secur_class.py
+++ selenium_secur_class.py
@@ -27,7 +27,6 @@ class MainPage_ChoosingProduct:
     def element_search(self):
 
         try:
-            
             self.driver.implicitly_wait(10)
             element_search = self.driver.find_element(By.ID, 'search')
             
@@ -42,57 +41,96 @@ class MainPage_ChoosingProduct:
     def choosing_product(self):
 
         try:
-            
             self.driver.implicitly_wait(10)
             self.driver.find_element(By.CLASS_NAME, 'product-wrap').click()
 
         except:
-            print('Element with the specified "class" was not found')
+            print('Element with the specified "CLASS_NAME" was not found')
             exit()
 
-# class SearchElementReview_WritingReview:
-#     def __init__(self, driver):
-#         self.driver = driver
+class SearchElementReview_WritingReview:
+    def __init__(self, driver):
+        self.driver = driver
 
-#     def search_element_review(self):
+    def search_element_review(self):
 
-#         try:
+        try:
+            self.driver.implicitly_wait(10)
+            tabs_manu = driver.find_element(By.LINK_TEXT, 'Отзывы')
+            tabs_manu.send_keys(Keys.RETURN)
 
-#             self.driver.implicitly_wait(10)
-#             tabs_manu = driver.find_element_by_link_text('Отзывы')
-#             tabs_manu.send_keys(Keys.RETURN)
+        except:
+            print('Element with the specified "link text" was not found')
+            exit()
 
-#         except:
-#             print('Element with the specified "link text" was not found')
-#             exit()
+    def writing_review(self):
+        try:
+            self.driver.implicitly_wait(10)
+            name = driver.find_element(By.ID, 'nickname_field')
 
-#     def writing_review(self):
-#         try:
+            name.clear()
+            name.send_keys('Vlad')
 
-#             name = driver.find_element_by_id('nickname_field')
-#             name.send_keys('Vlad')
+        except:
+            print('Element with the specified "ID" was not found')
+            exit()
 
-#             email = driver.find_element_by_id('summary_field')
-#             email.send_keys('@')
+        try:
+            self.driver.implicitly_wait(10)
+            email = driver.find_element(By.ID, 'summary_field')
 
-#             detail = driver.find_element_by_id('review_field')
-#             detail.send_keys('Very good')
+            email.clear()
+            email.send_keys('@')
 
-#             pluses_product = driver.find_element_by_id('pluses_product')
-#             pluses_product.send_keys('Very-very good')
+        except:
+            print('Element with the specified "ID" was not found')
+            exit()
+        
+        try:
+            self.driver.implicitly_wait(10)
+            detail = driver.find_element(By.ID, 'review_field')
 
-#             lows_product = driver.find_element_by_id('lows_product')
-#             lows_product.send_keys('Nope')
+            detail.clear()
+            detail.send_keys('Very good')
 
-#             driver.find_element_by_class_name('btn.btn-silver.btn-review-post.button').click()
+        except:
+            print('Element with the specified "ID" was not found')
+            exit()
 
-#         except Exception as ex:
-#             print(ex)
+        try:
+            self.driver.implicitly_wait(10)
+            pluses_product = driver.find_element(By.ID, 'pluses_product')
 
-        # finally:
-        #     time.sleep(10)
-        #     driver.close()
-        #     driver.quit()
+            pluses_product.clear()
+            pluses_product.send_keys('Very-very good')
+
+        except:
+            print('Element with the specified "ID" was not found')
+            exit()
+
+        try:
+            self.driver.implicitly_wait(10)
+            lows_product = driver.find_element(By.ID, 'lows_product')
+
+            lows_product.clear()
+            lows_product.send_keys('Nope')
+
+        except:
+            print('Element with the specified "ID" was not found')
+            exit()
+
+        try:
+            self.driver.implicitly_wait(10)
+            driver.find_element(By.CLASS_NAME, 'btn.btn-silver.btn-review-post.button').click()
+
+        except:
+            print('Element with the specified "CLASS_NAME" was not found')
+            exit()
+
+        finally:
+            time.sleep(10)
+            driver.close()
+            driver.quit()
 
 
 
@@ -102,8 +140,8 @@ first_class.main_page()
 first_class.element_search()
 first_class.choosing_product()
 
-# second_class = SearchElementReview_WritingReview(driver)
-# second_class.search_element_review()
-# second_class.writing_review()
+second_class = SearchElementReview_WritingReview(driver)
+second_class.search_element_review()
+second_class.writing_review()
 
 
